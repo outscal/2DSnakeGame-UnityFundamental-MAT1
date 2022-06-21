3893aadcb6130c227331257d08745ebd65cd316e
Aditya E Ajith <adityaeajith@gmail.com>
2022-06-22 03:18:05 +0530

    Added Snake Movement
    - Head Movement
    - Body Movement

---
+++ Co-Op Snake Game/Assets/_Project/Script/BodyService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeGame.Snake
+{
+	[System.Serializable]
+	public class BodyService
+	{
+		public BodyController bodyPrefab;
+		public Transform bodyCollection;
+		public int startCount;
+
+		private List<BodyController> m_bodies;
+		
+		internal void Move(Vector3 position)
+		{
+			if(IsBodyEmpty())
+				return;
+
+			for (int bodyIndex = m_bodies.Count-1; bodyIndex >= 1; bodyIndex--)
+			{
+				m_bodies[bodyIndex].setPosition(m_bodies[bodyIndex - 1].transform.position);
+			}
+
+			m_bodies[0].setPosition(position);
+		}
+
+		internal void ResetBody()
+		{
+			if (m_bodies == null)
+				m_bodies = new List<BodyController>();
+	
+			m_bodies.Clear();
+			for (int i = 0; i < startCount; i++)
+			{
+				CreateBody();
+			}
+		}
+
+		public void CreateBody()
+		{
+			BodyController newBody = Object.Instantiate(bodyPrefab);
+			newBody.transform.parent = bodyCollection;
+			m_bodies.Add(newBody);
+		}
+
+		public bool IsBodyEmpty()
+		{
+			return m_bodies.Count == 0 || m_bodies == null ;
+		}
+	}
+}
---
+++ Co-Op Snake Game/Assets/_Project/Script/SnakeController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeGame.Snake
+{
+	public class SnakeController : MonoBehaviour
+	{
+		public float speed;
+
+		[Serializable]
+		public struct InputKeyConfig
+		{
+			public KeyCode up;
+			public KeyCode left;
+			public KeyCode right;
+			public KeyCode down;
+		}
+
+		public InputKeyConfig keyConfig;
+
+		private Vector2 m_CurrentDirection;
+		private Vector2 m_NextDirection;
+		private float m_MoveTimer;
+
+		public BodyService bodyService;
+
+		private void Start()
+		{
+			m_CurrentDirection = Vector2.right;
+			m_NextDirection = Vector2.right;
+			bodyService.ResetBody();
+		}
+
+		private void Update()
+		{
+			GetInput();
+		}
+
+		private void GetInput()
+		{
+			if (Input.GetKeyDown(keyConfig.up))
+				SetDirection(Vector2.up);
+			else if (Input.GetKeyDown(keyConfig.left))
+				SetDirection(Vector2.left);
+			else if (Input.GetKeyDown(keyConfig.right))
+				SetDirection(Vector2.right);
+			else if (Input.GetKeyDown(keyConfig.down))
+				SetDirection(Vector2.down);
+		}
+
+		private void SetDirection(Vector2 direction)
+		{
+			m_NextDirection = direction;
+		}
+
+		private void FixedUpdate()
+		{
+			Movement();
+		}
+
+		private void Movement()
+		{
+			IncreaseTimer();
+			if (m_MoveTimer > (1 / speed))
+			{
+				ResetTimer();
+				MoveSnake();
+			}
+		}
+		private void IncreaseTimer() => m_MoveTimer += Time.fixedDeltaTime;
+		private void ResetTimer() => m_MoveTimer = 0;
+
+		private void MoveSnake()
+		{
+			bodyService.Move(transform.position);
+
+			if (m_CurrentDirection != -m_NextDirection)
+				m_CurrentDirection = m_NextDirection;
+			transform.position += (Vector3)m_CurrentDirection;
+		}
+	}
+}
