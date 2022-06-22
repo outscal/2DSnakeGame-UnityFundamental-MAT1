e4e3a90fc8d5d70a139755c64dd637eb152f5aa0
Aditya E Ajith <adityaeajith@gmail.com>
2022-06-22 05:36:16 +0530

    Added boundary

---
+++ Co-Op Snake Game/Assets/_Project/Script/BoundController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeGame
+{
+    public struct Boundary
+	{
+        public int boundMinX;
+        public int boundMaxX;
+		public int boundMinY;
+		public int boundMaxY;
+	}
+
+    public class BoundController : MonoBehaviour
+    {
+
+        public static Boundary bounds;
+
+		private BoxCollider2D col; 
+
+		private void Awake()
+		{
+			bounds = new Boundary();
+			col = GetComponent<BoxCollider2D>();
+			IntializeBounds();
+		}
+
+		private void IntializeBounds()
+		{
+			bounds.boundMaxX = (int)(col.size.x/2); 
+			bounds.boundMinX = - bounds.boundMaxX;
+			bounds.boundMaxY = (int)(col.size.y/2);
+			bounds.boundMinY = - bounds.boundMaxY;
+		}
+	}
+}
